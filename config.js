aadd9e7a900d0a20a4b1846643b6ee31239a2fe0
MuhamedSaber1990 <111927119+MuhamedSaber1990@users.noreply.github.com>
2025-06-04 12:09:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 export const GOOGLE_CLIENT_ID =
   "91322746968-v9q2esraihddteh4geo2ke47jsg8n0b1.apps.googleusercontent.com";
 export const GOOGLE_CLIENT_SECRET = "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";
-export const GOOGLE_REDIRECT_URI = "http://localhost:3000/auth/google/callback";
+export const GOOGLE_REDIRECT_URI = "http://84.32.188.34:3000/auth/google/callback";
