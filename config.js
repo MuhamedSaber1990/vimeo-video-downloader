092aba9f0bb2a9c6dcdacd7db7ea75f780aa1f60
MuhamedSaber1990 <111927119+MuhamedSaber1990@users.noreply.github.com>
2025-06-06 14:28:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 export const GOOGLE_CLIENT_ID =
-  "91322746968-v9q2esraihddteh4geo2ke47jsg8n0b1.apps.googleusercontent.com";
-export const GOOGLE_CLIENT_SECRET = "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";
-export const GOOGLE_REDIRECT_URI = "http://84.32.188.34.nip.io:3000/auth/google/callback";
+  "add yours here";
+export const GOOGLE_CLIENT_SECRET = "add yours here";
+export const GOOGLE_REDIRECT_URI = "add google redirect url";
